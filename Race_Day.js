39ed8fefa5bdce0d5141b973f1c65598857b2143
Wambua <ley9@cdc.gov>
2024-11-18 14:10:45 -0500

    added RaceDay

---
+++ Race_Day.js
@@ -0,0 +1,21 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly = true;
+const age = 18;
+if (age > 18 && registeredEarly === true){
+
+}
+if (age > 18 && registeredEarly ===true )
+{
+  console.log(`You will tace at 9:30 am and your racenumber is ${raceNumber}`);
+}
+else if (raceNumber > 18 && registeredEarly === false) 
+ 
+{
+   console.log(`You will raise at 11:0 0am and your race nuber is ${raceNumber}`);
+}
+else if (age < 18){
+  console.log(`You will race at 12:30 pm and your race nuber is ${raceNumber}`);
+}
+else if (age === 18){
+  console.log(`Come to the help desk`);
+}
